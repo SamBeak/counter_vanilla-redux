458244830b11304a20d5a45e020d7067055a320a
Baek HyunGyu <baekhg102@gmail.com>
2023-07-14 14:16:34 +0900

    docs: Feat count number function
    add count number function to compare with redux and vanilla

---
+++ src/components/WrapComponent.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import CountComponent from './count/CountComponent'
+
+export default function WrapComponent() {
+  return (
+    <CountComponent />
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import './scss/style.scss';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 순수 Vanilla JS로 구현한 숫자 증감 기능 => 매번 업데이트를 인지시켜줘야하는 번거로움이 있었네.
+const add = document.getElementById('add');
+const minus = document.getElementById('minus');
+const number = document.querySelector('span');
+
+let cnt = 0;
+
+const updateText = () => {
+    number.innerText = cnt;
+};
+
+const handleAdd = () => {
+    cnt = cnt + 1;
+    updateText();
+};
+
+const handleMinus = () => {
+    cnt = cnt - 1;
+    updateText();
+};
+
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);
